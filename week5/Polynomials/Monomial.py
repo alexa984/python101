c48b717bc39bf3c00bbbad63d58db5d2237c5b48
alexa984 <alex.yovkova@gmail.com>
2019-04-03 11:51:58 +0300

    some bugged files removed for upgrade

--- week5/Polynomials/Monomial.py
+++
@@ -1,8 +0,0 @@
-class Monome:
-
-    def __init__(self, coeff = 1, power = 1):
-        self.coeff = coeff
-        self.power = power
-
-    def clc_derivative(self):
-        return Monomial(coeff=self.coeff*power, power-1)
